c604d43fac5a27372801810379d3d6c5c94b08b6
Byron <byron@attoconsulting.com>
2019-11-12 14:48:25 -0500

    some barebones skeletons on here

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Word Bank!</h1>
+        <Input />
+        <Button />
+        <WordBank word="shoe" definition="a thing you wear." /> 
+      </div>
+    );
+  }
+}
+
+
+
+class Input extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <input type="text" class="form-control" placeholder="Search for word" />
+    );
+  }
+}
+
+
+class Button extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render(){
+    return(
+      <button>Get word</button>
+    );
+  }
+}
+
+
+
+class WordBank extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      word: this.props.word,
+      definition: this.props.definition
+    };
+  }
+  render(){
+    return(
+      <div>
+        <div class="container">
+          <div class="row">word: {this.state.word}</div>
+          <div class="row">definition: {this.state.definition}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
